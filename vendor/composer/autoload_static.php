110356de5651426deb9275f62708e0fd965aa836
Edilson Xavier <edilsonx@gmail.com>
2019-03-27 22:32:18 -0300

    Site login

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,6 +7,10 @@ namespace Composer\Autoload;
 class ComposerStaticInit391018df4f579b69fabd45119ba640f2
 {
     public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
         'H' => 
         array (
             'Hcode\\' => 6,
@@ -14,6 +18,10 @@ class ComposerStaticInit391018df4f579b69fabd45119ba640f2
     );
 
     public static $prefixDirsPsr4 = array (
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
         'Hcode\\' => 
         array (
             0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
@@ -37,24 +45,12 @@ class ComposerStaticInit391018df4f579b69fabd45119ba640f2
         ),
     );
 
-    public static $classMap = array (
-        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
-        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
-        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
-        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
-        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
-        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
-        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
-        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit391018df4f579b69fabd45119ba640f2::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit391018df4f579b69fabd45119ba640f2::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit391018df4f579b69fabd45119ba640f2::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit391018df4f579b69fabd45119ba640f2::$classMap;
 
         }, null, ClassLoader::class);
     }
--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -1,109 +1,123 @@
-<?php
-
+<?php 
+ 
 namespace Hcode;
 
 use Rain\Tpl;
 
-class Mailer {
+use PHPMailer\PHPMailer\PHPMailer;
 
+class Mailer {
+ 
 	const USERNAME = "montserrat.tecnologia@gmail.com";
 	const PASSWORD = "<?montserrattech20190214?>";
 	const NAME_FROM = "Montserrat Tecnologia";
-
+ 
 	private $mail;
-
-	public function __construct($toAddress, $toName, $subject, $tplName, $data = array())
-	{
+ 
+	public function __construct($toAddress, $toName, $subject, $tplName, $data = array()){
+ 
 		$config = array(
-			"tpl_dir" 		=> $_SERVER["DOCUMENT_ROOT"]."/views/email/",
-			"cache_dir"		=> $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
-			"debug"			=> false
+			"tpl_dir"=>$_SERVER["DOCUMENT_ROOT"]."/views/email/",
+			"cache_dir"=>$_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			"debug"=>false
 		);
-
-		Tpl::configure( $config );
-
+ 
+		Tpl::configure($config);
+ 
 		$tpl = new Tpl;
-		//$this->tpl = new Tpl;
-
-		foreach ($data as $key => $value) 
-		{
+ 
+		foreach ($data as $key => $value) {
 			$tpl->assign($key, $value);
-			//$this->tpl->assign($key, $value);
 		}
-
+ 
 		$html = $tpl->draw($tplName, true);
-		//$html = $this->tpl->draw($tplName, true);
-
-		$this->mail = new \PHPMailer;
-
+ 
+		//Create a new PHPMailer instance
+		$this->mail = new PHPMailer();
+ 
+		//Tell PHPMailer to use SMTP
 		$this->mail->isSMTP();
-
-		$this->mail->SMTPDebug = 0;
-		//$this->mail->SMTPDebug = 2;
-
-		$this->mail->Debugoutput = 'html';
-
-		$this->mail->Host = 'smtp.gmail.com';
-
-		$this->mail->Port = 587;
-		//$this->mail->Port = 465;
-
-		//===== NOVO TRECHO ==========
-		$this->mail->isSMTP();
-		
-		$this->mail->SMTPOptions = array(
+ 
+ 		$this->mail->SMTPOptions = array(
     		'ssl' => array(
         	'verify_peer' => false,
         	'verify_peer_name' => false,
         	'allow_self_signed' => true
-    		));
-    	
-		//======FIM DO NOVO TRECHO ======
-		
+    	));
+
+		//Enable SMTP debugging
+		// 0 = off (for production use)
+		// 1 = client messages
+		// 2 = client and server messages
+		$this->mail->SMTPDebug = 0;
+ 
+		//Set the hostname of the mail server
+		$this->mail->Host = 'smtp.gmail.com';
+		// use
+		// $this->mail->Host = gethostbyname('smtp.gmail.com');
+		// if your network does not support SMTP over IPv6
+ 
+		//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+		$this->mail->Port = 587;
+ 
+		//Set the encryption system to use - ssl (deprecated) or tls
 		$this->mail->SMTPSecure = 'tls';
-		//$this->mail->SMTPSecure = 'ssl';
-
+ 
+		//Whether to use SMTP authentication
 		$this->mail->SMTPAuth = true;
-
+ 
+		//Username to use for SMTP authentication - use full email address for gmail
 		$this->mail->Username = Mailer::USERNAME;
-
-		$this->mail->Passord = Mailer::PASSWORD;
-
+ 
+		//Password to use for SMTP authentication
+		$this->mail->Password = Mailer::PASSWORD;
+ 
+		//Set who the message is to be sent from
 		$this->mail->setFrom(Mailer::USERNAME, Mailer::NAME_FROM);
-
+ 
+		//Set an alternative reply-to address
+		// $this->mail->addReplyTo('replyto@example.com', 'First Last');
+ 
+		//Set who the message is to be sent to
 		$this->mail->addAddress($toAddress, $toName);
-		//$this->mail->addAddress('montserrat.tecnologia@gmail.com', 'edilsonx');
-		
+ 
+		//Set the subject line
 		$this->mail->Subject = $subject;
-
+ 
+		//Read an HTML message body from an external file, convert referenced images to embedded,
+		//convert HTML into a basic plain-text alternative body
 		$this->mail->msgHTML($html);
-
-		$this->mail->AltBody = 'This is a plain-text message body';		
-
-/*
-		var_dump("=============INICIO MAILER PHP ================================================");
-		var_dump($this->mail);
-		var_dump("=============FIM MAILER PHP ================================================");
-		exit;
-
-*/
+ 
+		//Replace the plain text body with one created manually
+		$this->mail->AltBody = 'This is a plain-text message body';
+ 
+		//Attach an image file
+		// $this->mail->addAttachment('images/phpmailer_mini.png');
+ 
+		//Section 2: IMAP
+		//IMAP commands requires the PHP IMAP Extension, found at: https://php.net/manual/en/imap.setup.php
+		//Function to call which uses the PHP imap_*() functions to save messages: https://php.net/manual/en/book.imap.php
+		//You can use imap_getmailboxes($imapStream, '/imap/ssl') to get a list of available folders or labels, this can
+		//be useful if you are trying to get this working on a non-Gmail IMAP server.
+		// function save_mail($this->mail)
+		// {
+		//     //You can change 'Sent Mail' to any other folder or tag
+		//     $path = "{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail";
+ 
+		//     //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
+		//     $imapStream = imap_open($path, $this->mail->Username, $this->mail->Password);
+ 
+		//     $result = imap_append($imapStream, $path, $this->mail->getSentMIMEMessage());
+		//     imap_close($imapStream);
+ 
+		//     return $result;
+		// }
 	}
-
-	public function send()
-	{	
-
-	/*
-		var_dump("=============INICIO MAILER SEND  ================================================");
-		var_dump($this->mail);
-		var_dump("=============FIM MAILER SEND  ================================================");
-		exit;
-	*/
-
+ 
+	public function send(){
+ 
 		return $this->mail->send();
-
 	}
-
-
 }
-
-?>
+ 
+ ?>
